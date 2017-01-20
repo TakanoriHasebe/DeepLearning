3f727d6f2b66bbe564eb02f8907a4d588c0da161
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-01-20 19:24:46 +0900

    simple_net directory pass is added

---
+++ ManufactureDeepLearning/NeuralNetwork/SimpleNet/gradient_simplenet.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 19:07:19 2017
+
+@author: Takanori
+"""
+
+"""
+ニューラルネットワークを例にして, 勾配を求めるプログラム
+"""
+
+import sys
+sys.path.append('../../')
+import numpy as np
+from common.gradientfunctions import numerical_gradient
+from common.outputactivationfunctions import softmax
+
+
+
+
+
+
+
+
+
+

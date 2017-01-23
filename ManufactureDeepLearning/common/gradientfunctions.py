d1eae461ca6cee5a8e121901fb118f127c0258bd
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-01-23 15:08:29 +0900

    gradient_simplenet is added

--- ManufactureDeepLearning/common/gradientfunctions.py
+++ ManufactureDeepLearning/common/gradientfunctions.py
@@ -9,7 +9,7 @@ Created on Fri Jan 20 11:55:43 2017
 """
 勾配法を用いる関数が記述してある
 """
-
+'''
 import numpy as np
 
 # 勾配を計算する関数
@@ -32,4 +32,59 @@ def numerical_gradient(f, x):
         x[idx] = tmp_val # 値を元に戻す
         
     return grad
+'''
+
+import numpy as np
+
+def _numerical_gradient_1d(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 値を元に戻す
+        
+    return grad
+
+
+def numerical_gradient_2d(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+        
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_1d(f, x)
+        
+        return grad
+
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 値を元に戻す
+        it.iternext()   
+        
+    return grad
+
+
 

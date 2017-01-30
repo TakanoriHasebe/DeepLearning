ba60acb84488b1d94c33fc18dabe1017496d0e12
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-01-30 15:06:09 +0900

    layer_naive.py is added and Layer/README is updated

---
+++ ManufactureDeepLearning/BackPropagation/Layer/layer_naive.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 14:56:45 2017
+
+@author: Takanori
+"""
+
+"""
+このプログラムはゼロから作るDeep Learningを元に作成されている
+乗算レイヤと加算レイヤに分けられている
+"""
+
+class MulLayer:
+    
+    # 以下でx, yの初期化を行なっている
+    # これらは, 順伝搬時の入力値を保持するために行う
+    def __init__(self):
+        self.x = None
+        self.y = None
+        
+    # 順伝搬の計算
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+        
+        return out
+        
+    # 逆伝搬の計算
+    ## 自分のプログラムでは, クラス内の変数から出力値を参照したが, 教科書のプログラムでは出力値を引数で受け取っている ##
+    def backward(self, dout):
+        dx = dout * self.y # xとyをひっくり返す
+        dy = dout * self.x 
+        
+        return dx, dy
+
+
+

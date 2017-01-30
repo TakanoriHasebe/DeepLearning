b45ad94eca54fce271e44839c21df42a1927e7f6
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-01-30 17:56:28 +0900

    program and README.md is updated

--- ManufactureDeepLearning/BackPropagation/Layer/layer_naive.py
+++ ManufactureDeepLearning/BackPropagation/Layer/layer_naive.py
@@ -11,10 +11,12 @@ Created on Mon Jan 30 14:56:45 2017
 乗算レイヤと加算レイヤに分けられている
 """
 
+# 誤差逆伝搬の乗算クラスの実装
 class MulLayer:
     
     # 以下でx, yの初期化を行なっている
     # これらは, 順伝搬時の入力値を保持するために行う
+    ## 本当に以下の記述で, 入力値が保存されているかということ ##
     def __init__(self):
         self.x = None
         self.y = None
@@ -34,6 +36,16 @@ class MulLayer:
         dy = dout * self.x 
         
         return dx, dy
+
+# 誤差逆伝搬法の加算レイヤの実装
+
+
+
+
+
+
+
+
 
 
 

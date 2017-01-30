0ea9f18f2c38bc7406fc76fa136281bbdd410949
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-01-30 18:44:20 +0900

    add_mul_layer.py is added

---
+++ ManufactureDeepLearning/BackPropagation/Layer/add_mul_layer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 18:41:13 2017
+
+@author: Takanori
+"""
+
+"""
+ゼロから作るDeep LearningのP.140の
+りんご２個とみかん３個の図を参考に作成された
+"""
+
+
+
+
+

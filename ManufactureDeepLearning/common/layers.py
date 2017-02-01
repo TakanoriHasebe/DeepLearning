87f5c070b6f25d92cec0be7b27908fe334f1f82e
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-02-01 12:04:39 +0900

    Layer/README and sigmoid are updated and temp folder is added

--- ManufactureDeepLearning/common/layers.py
+++ ManufactureDeepLearning/common/layers.py
@@ -14,6 +14,28 @@ Created on Tue Jan 31 11:59:37 2017
 
 import numpy as np
 
+class Relu:
+    # 初期化
+    # mask変数はTrue/FalseからなるNumpy配列であり, 0以下でTrue, 0以上でFalseとなる
+    def __init__(self):
+        self.mask = None
+    
+    # 順伝搬    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        
+        return out
+           
+    # 逆伝搬
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        
+        return dx
+        
+
 
 
 
---
+++ ManufactureDeepLearning/temp/temp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 12:00:46 2017
+
+@author: Takanori
+"""
+
+"""
+temp
+"""
+
+# sigmoid関数を多次元配列に対応させるために, numpyの使い方を確かめる
+
+
+
+

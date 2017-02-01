4c0f1937e0965a8c5d334e4286f1fa116c0c2f74
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-02-01 12:09:40 +0900

    tempプログラムが更新された。

--- ManufactureDeepLearning/temp/temp.py
+++ ManufactureDeepLearning/temp/temp.py
@@ -10,8 +10,11 @@ Created on Wed Feb  1 12:00:46 2017
 temp
 """
 
+import numpy as np
+import math
+
 # sigmoid関数を多次元配列に対応させるために, numpyの使い方を確かめる
+x = np.array([1, 2, 3])
 
-
-
-
+y = 1/(1 + np.exp(-x))
+print(y)

e168b76f729d1246179c24c1518d3bcd378980b7
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-02-02 11:30:12 +0900

    Affine変換のレイヤが追加された

--- ManufactureDeepLearning/common/layers.py
+++ ManufactureDeepLearning/common/layers.py
@@ -14,6 +14,7 @@ Created on Tue Jan 31 11:59:37 2017
 
 import numpy as np
 
+# ReLUレイヤ
 class Relu:
     # 初期化
     # mask変数はTrue/FalseからなるNumpy配列であり, 0以下でTrue, 0以上でFalseとなる
@@ -35,10 +36,25 @@ class Relu:
         
         return dx
         
+# Sigmoidレイヤ
+class Sigmoid:
+    
+    # 変数の初期化
+    def __init__(self):
+        self.out = None
 
-
-
-
+    # 順伝搬
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out # 値を保持する
+        
+        return out
+    
+    # 逆伝搬
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        
+        return dx
 
 
 

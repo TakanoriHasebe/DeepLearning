85d8baf54eebf58e482b9bdb6dfe0af455f7d825
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-02-17 11:10:55 +0900

    ハイパーパラメータを可視化するプログラムの追加

---
+++ ManufactureDeepLearning/search-hyper-parameter/temp_hyper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 09:35:29 2017
+
+@author: Takanori
+"""
+
+"""
+ハイパーパラメーターの探索を行うプログラムのtemp
+"""
+
+import numpy as np
+
+weight_decay = 10 ** np.random.uniform(-8, -4)
+print(weight_decay)
+

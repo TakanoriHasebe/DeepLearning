7dd082c8afee2c750db2598be05f868d94b5bc31
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-02-19 14:28:35 +0900

    README.mdの更新

---
+++ ManufactureDeepLearning/make-neural-network/5-layer-neural-network.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 14:25:35 2017
+
+@author: Takanori
+"""
+
+"""
+MNISTを誤差逆伝搬法を用いて学習するネットワーク
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+

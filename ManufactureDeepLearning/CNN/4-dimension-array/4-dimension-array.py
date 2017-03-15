54609e054219bd89315dab67d8081f1470fd7b8d
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-03-15 16:50:42 +0900

    im2colプログラムを書いた

---
+++ ManufactureDeepLearning/CNN/4-dimension-array/4-dimension-array.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 15:15:09 2017
+
+@author: Takanori
+"""
+
+"""
+4次元配列
+"""
+
+import numpy as np
+
+x = np.random.rand(10, 1, 28, 28)
+
+y = np.random.rand(2, 2, 2, 1)
+
+
+
---
+++ ManufactureDeepLearning/CNN/convolution-class/convolution-layer.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 15:57:54 2017
+
+@author: Takanori
+"""
+
+"""
+畳み込み層の実装
+"""
+
+import numpy as np
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    """
+    Parameters
+    ----------
+    input_data : (データ数, チャンネル, 高さ, 幅)の4次元配列からなる入力データ
+    filter_h : フィルターの高さ
+    filter_w : フィルターの幅
+    stride : ストライド
+    pad : パディング
+    Returns
+    -------
+    col : 2次元配列
+    """
+    N, C, H, W = input_data.shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+
+    img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+    # print(img.shape)
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+    # print(col.shape)
+
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
+    return col
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+        
+        # 中間データ（backward時に使用）
+        self.x = None   
+        self.col = None
+        self.col_W = None
+        
+        # 重み・バイアスパラメータの勾配
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+
+
+
+
